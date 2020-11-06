a853bab2f7e82f9dfbf904542a87ff388b24059f
yugaro <yugaro.yugaro@gmail.com>
2020-11-07 01:01:29 +0900

    Create permutation

---
+++ permutation/permutation.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, x, n) for (ll i = x; i < (ll)(n); i++)
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define INF 1e9
+
+ll gcd(ll a, ll b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+const int mod = 1000000007;
+struct mint
+{
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+    mint operator-() const { return mint(-x); }
+    mint &operator+=(const mint a)
+    {
+        if ((x += a.x) >= mod)
+            x -= mod;
+        return *this;
+    }
+    mint &operator-=(const mint a)
+    {
+        if ((x += mod - a.x) >= mod)
+            x -= mod;
+        return *this;
+    }
+    mint &operator*=(const mint a)
+    {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint a) const { return mint(*this) += a; }
+    mint operator-(const mint a) const { return mint(*this) -= a; }
+    mint operator*(const mint a) const { return mint(*this) *= a; }
+    mint pow(ll t) const
+    {
+        if (!t)
+            return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1)
+            a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod - 2); }
+    mint &operator/=(const mint a) { return *this *= a.inv(); }
+    mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream &operator>>(istream &is, mint &a) { return is >> a.x; }
+ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+
+struct combination
+{
+    vector<mint> fact, ifact;
+    combination(int n) : fact(n + 1), ifact(n + 1)
+    {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i)
+            fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i)
+            ifact[i - 1] = ifact[i] * i;
+    }
+    mint operator()(int n, int k)
+    {
+        if (k < 0 || k > n)
+            return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+};
+
+mint nPk(int n, int k)
+{
+    //n=k→nPn=n!
+    if (n < 0)
+        return 0;
+    combination nCk(n);
+    mint res = nCk(n, k);
+    res *= nCk.fact[k];
+    return res;
+}
