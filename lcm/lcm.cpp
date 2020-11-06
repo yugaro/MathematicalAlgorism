35f8a72a64aea8dfa3bca0a6250c42916d2d7522
yugaro <yugaro.yugaro@gmail.com>
2020-11-07 01:00:52 +0900

    Create lcm

---
+++ lcm/lcm.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, x, n) for (ll i = x; i < (ll)(n); i++)
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define INF 1e9
+
+ll gcd(ll a, ll b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
